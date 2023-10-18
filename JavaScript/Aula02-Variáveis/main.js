94ff8d9f5135685d60b5fedade92f03a0fecb90d
Annalldc <annaclaralimadiniz@hotmail.com>
2023-10-17 21:42:25 -0300

    JS

---
+++ JavaScript/Aula02-Variáveis/main.js
@@ -0,0 +1,33 @@
+var nomeDoPrimeiroAluno = "Felipe";
+console.log(nomeDoPrimeiroAluno);
+
+let numeroDeAprovados = 5;
+console.log(numeroDeAprovados);
+
+console.clear();
+
+var nomeAluno = "Felipe";
+console.log(nomeAluno);
+
+var nomeAluno = "Anna"
+console.log(nomeAluno);
+
+//Exibir soma de dois números
+//console.log(2+4)
+
+var numero1 = 2;
+var numero2 = 4;
+
+var soma = numero1 + numero2;
+console.log(soma);
+
+//String
+var texto = "Eu gosto de hambúrguer ";
+var texto2 = "Eu gosto de batata frita";
+console.log(texto + texto2);
+
+//Concatenar -  ato de juntar duas ou mais Strings
+var Concatenar = texto + "\n" + texto2;
+console.log(Concatenar);
+
+
