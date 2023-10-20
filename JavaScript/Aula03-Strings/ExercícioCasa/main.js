234964f4f8e20ca07a91eae132f88b8bacd7293d
Annalldc <annaclaralimadiniz@hotmail.com>
2023-10-19 21:44:52 -0300

    AAAAA

---
+++ JavaScript/Aula03-Strings/ExercícioCasa/main.js
@@ -0,0 +1,2 @@
+const string = "Estamos aprendendo JavaScript e seremos futuros Devs"
+console.log("posição 19" + string.charAt(19));
--- JavaScript/Aula03-Strings/main.js
+++ JavaScript/Aula03-Strings/main.js
@@ -24,6 +24,7 @@ console.log(string1.charAt(8));
 
 console.clear();
 
+//posição caractere a partir do 0 e conta os espaços
 const string3 = "Instituto da oportunidade Social";
 console.log(string3);
 console.log("posição 10" + string3.charAt(11));
@@ -31,6 +32,32 @@ console.log("posição 11" + string3.charAt(5));
 
 console.clear
 
+//quantos caracteres
 const stringA = "Instituto da oportunidade Social";
 console.log("Quantidade de caracteres: " + stringA.length);
+
+console.clear()
+
+//maiuscula o minuscula
+const string5 = "Todo dia esse ser humano fica..."
+const string9 = "não aguento mais conviver no mesmo ambiente"
+
+console.log("Trazer tudo em minusculo: " + string5.toLocaleLowerCase());
+console.log("Trazer tudo em maiusculo: " + string9.toLocaleUpperCase());
+
+console.clear()
+
+//troca de texto
+let mensagem = "Que saco!";
+console.log(mensagem);
+
+let novaMensagem = mensagem.replace('saco' , 'legal');
+console.log(novaMensagem)
+
+var trocaTexto = "Bem vindo ao Valorant"
+let textoTrocado = trocaTexto.replace("Bem vindo ao Valorant" , "The Sims");
+
+console.log("Texto Original: " + trocaTexto);
+console.log("Texto alterado: " + textoTrocado);
+
 
