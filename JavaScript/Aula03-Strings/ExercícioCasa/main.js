63949912557eb5830a3c457d82a413935ec9fe7c
Annalldc <annaclaralimadiniz@hotmail.com>
2023-10-22 17:38:20 -0300

    Atutu

--- JavaScript/Aula03-Strings/ExercícioCasa/main.js
+++ JavaScript/Aula03-Strings/ExercícioCasa/main.js
@@ -1,2 +1,6 @@
-const string = "Estamos aprendendo JavaScript e seremos futuros Devs"
-console.log("posição 19" + string.charAt(19));
+var text = "Estamos aprendendo JavaScript e seremos futuros Devs"
+
+console.log("Posição 19: " + text.charAt(19))
+console.log("Quantidade de caracteres: " + text.length)
+console.log("Caracteres em maiúsculo: " + text.toUpperCase())
+console.log(text.substring(48))
