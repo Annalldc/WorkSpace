a715547d0b6b673e57e7e726c4a4c85b59bbf931
Annalldc <annaclaralimadiniz@hotmail.com>
2023-10-25 21:36:45 -0300

    Atualizão

--- JavaScript/Aula05-Laçosderepeticao/main.js
+++ JavaScript/Aula05-Laçosderepeticao/main.js
@@ -1,15 +1,21 @@
 //somar
-let contador = 1
+// let contador = 1
 
-while(contador <= 10){
-    console.log(`O número do contador é ${contador}`);
-    contador++;
-}
+// while(contador <= 10){
+//     console.log(`O número do contador é ${contador}`);
+//     contador++;
+// }
 
-console.clear()
 
-let contador = 1
-do{
-    debugger
+
+// let contador = 1
+// do{
+//     debugger
+//     console.log(`O número do contador é ${contador}`)
+// }while(contador <= 10)
+
+//laço for
+
+for(let contador = 1; contador <= 10; contador++){
     console.log(`O número do contador é ${contador}`)
-}while(contador <= 10)
+}
