37554b1ea2f16a5c0e925e681e360cd7e227b9fe
Annalldc <annaclaralimadiniz@hotmail.com>
2023-11-01 19:16:04 -0300

    add

--- JavaScript/Aula08-Objetos/main.js
+++ JavaScript/Aula08-Objetos/main.js
@@ -1,12 +1,22 @@
-// Funções
-function addNums(num1 = 1, num2 = 1) {
-    return num1 + num2;
-   }
-   let x = addNums(4, 5); // Chamada da função addNums
-   console.log(x);
-   let z = myFunc(4, 5); // Chamada da função myFunc
-   console.log(z);
-   function myFunc(num1, num2) {
+function somaNumeros (num1 = 1, num2 = 1){
+    return num1 + num2
+}
+
+let x = somaNumeros(4,5); //Chamada da função AddNums
+console.log(x);
+
+let z = myFunc(4,5); // Chamda da função MyFunc
+console.log(z);
+
+function myFunc(num1,num2){
     return num1 * num2;
-   }
-   
+}
+
+let soma = somaNumeros(10,15,3)
+console.log("Essa é a variável soma:" + soma)
+
+console.clear()
+
+const hello = () => {
+    return 'Olá Arrow Function!';
+}
