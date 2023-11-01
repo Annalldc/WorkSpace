68d7d01dba63b7975ef94e79d7a2f3f64daf06c3
Annalldc <annaclaralimadiniz@hotmail.com>
2023-11-01 20:53:42 -0300

    Add

--- JavaScript/Aula08-Objetos/main.js
+++ JavaScript/Aula08-Objetos/main.js
@@ -1,22 +1,66 @@
-function somaNumeros (num1 = 1, num2 = 1){
-    return num1 + num2
-}
+// function somaNumeros(num1 = 1, num2 = 1) {
+//   return num1 + num2;
+// }
 
-let x = somaNumeros(4,5); //Chamada da função AddNums
-console.log(x);
+// let x = somaNumeros(4, 5); //Chamada da função AddNums
+// console.log(x);
 
-let z = myFunc(4,5); // Chamda da função MyFunc
-console.log(z);
+// let z = myFunc(4, 5); // Chamda da função MyFunc
+// console.log(z);
 
-function myFunc(num1,num2){
-    return num1 * num2;
-}
+// function myFunc(num1, num2) {
+//   return num1 * num2;
+// }
 
-let soma = somaNumeros(10,15,3)
-console.log("Essa é a variável soma:" + soma)
+// let soma = somaNumeros(10, 15, 3);
+// console.log("Essa é a variável soma:" + soma);
 
-console.clear()
+// console.clear();
 
-const hello = () => {
-    return 'Olá Arrow Function!';
-}
+// const hello = () => {
+//   return "Olá Arrow Function!";
+// };
+// console.log(hello());
+// console.log(addNums(4, 8, 8));
+
+// function imparPar(num) {
+//   if (num % 2 == 0) {
+//     return "Par";
+//   } else if (num % 2 == 1) {
+//     return `o número ${num} é impar`;
+//   } else {
+//     return `Não consegui identificar se é impar ou par`;
+//   }
+// }
+
+// let recebePrimeiroNumero = imparPar(5);
+// console.log(recebePrimeiroNumero);
+
+// let recebeSegundoNumero = imparPar(5.75);
+// console.log(recebeSegundoNumero);
+
+// var contexto = 5.75 % 2;
+// console.log(contexto);
+
+// console.clear();
+
+// const celular = {
+//   marca: Apple,
+//   modelo: "Iphone 15 pro Max",
+//   cor: "Rosa",
+//   mamoria: "512gb",
+//   memoriaRam: "16gb",
+// };
+
+// console.log(celular);
+
+console.clear();
+
+//Eventos
+const boasVindas = () => {
+  alert("Bem vindo a nossa página");
+  console.log("Bem vindo a nossa página");
+};
+const eventClique = () => {
+  console.log("Você clicou no botão");
+};
