3e2cac4e36190e2ed026ace2d1959ff967b2df8f
Annalldc <annaclaralimadiniz@hotmail.com>
2023-11-07 21:39:58 -0300

    Add

--- JavaScript/Aula08-Objetos/main.js
+++ JavaScript/Aula08-Objetos/main.js
@@ -56,11 +56,24 @@
 
 console.clear();
 
-//Eventos
+// //Eventos
+// const boasVindas = () => {
+//   alert("Bem vindo a nossa página");
+//   console.log("Bem vindo a nossa página");
+// };
+// const eventClique = () => {
+//   console.log("Você clicou no botão");
+// };
+
+console.clear();
 const boasVindas = () => {
-  alert("Bem vindo a nossa página");
-  console.log("Bem vindo a nossa página");
+alert('Bem vindo a nossa página');
+console.log('Bem vindo a nossa página');
 };
 const eventClique = () => {
-  console.log("Você clicou no botão");
+console.log('Você clicou no botão');
 };
+const mouseEmCima = () => {
+console.log('Mouse está em cima do título');
+};
+
---
+++ JavaScript/Aula09-ManipularArrays/main.js
@@ -0,0 +1,21 @@
+// Método forEach()
+// const frutas = ["apple", "orange", "cherry"];
+// frutas.forEach(minhaFuncao);
+
+// function minhaFuncao(item, index) {
+
+//   console.log(`Índice: ${index}`);
+//   console.log(`Elemento: ${item}`);
+
+// }
+
+console.clear()
+
+// Método map()
+const numeros = [4, 9, 16, 25];
+const newArray = numeros.map(Math.sqrt);
+console.clear();
+console.log('Números:');
+console.log(numeros);
+console.log('Novo array:');
+console.log(newArray);
