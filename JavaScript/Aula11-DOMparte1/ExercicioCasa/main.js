13624182e28576b7ffd53103ef006babedcc5888
Annalldc <annaclaralimadiniz@hotmail.com>
2023-11-08 21:17:14 -0300

    Add

--- JavaScript/Aula11-DOMparte1/ExercicioCasa/main.js
+++ JavaScript/Aula11-DOMparte1/ExercicioCasa/main.js
@@ -2,4 +2,4 @@ const elementoH1 =  document.createElement('h1');
 elementoH1.textContent = 'Cadastro de usuário';
 
 document.body.appendChild(elementoH1);
-elementoH1.innerHTML = 'Cadastro empresa Macron';
+document.title = 'Cadastro empresa Macron';
