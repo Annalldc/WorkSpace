da6bdf96fe453310b202280d0504bef181106562
Annalldc <annaclaralimadiniz@hotmail.com>
2023-11-08 21:14:01 -0300

    Add

---
+++ JavaScript/Aula11-DOMparte1/ExercicioCasa/main.js
@@ -0,0 +1,5 @@
+const elementoH1 =  document.createElement('h1');
+elementoH1.textContent = 'Cadastro de usuário';
+
+document.body.appendChild(elementoH1);
+elementoH1.innerHTML = 'Cadastro empresa Macron';
---
+++ JavaScript/Aula11-DOMparte1/main.js
@@ -0,0 +1,8 @@
+// Criação da variável e criação do botão
+let botao = document.createElement("BUTTON");
+
+document.body.appendChild(botao)
+
+botao.innerHTML = 'Clique agora!';
+
+botao.style.borderBottom = 'solid 2px pink';
